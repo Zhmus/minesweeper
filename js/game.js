365bcdd9dd0e493da38a7a67f531613ba4aad1ec
alexandra <alexandra.zhmutskaya@pactera.com>
2019-06-04 20:48:10 +0300

    main - first version game - second version

---
+++ js/game.js
@@ -0,0 +1,308 @@
+'use strict';
+
+let MineSweeper = (function () {
+
+    let game;
+
+    class GameField {
+
+        constructor(fieldWidth, fieldHeight, bombNumber) {
+            this.fieldWidth = fieldWidth;
+            this.fieldHeight = fieldHeight;
+            this.bombNumber = bombNumber;
+        }
+
+        bindClick(...self) {
+            const _self = self[0];
+            const currentCell = {x: +this.id.split('-')[0], y: +this.id.split('-')[1]};
+
+            if (!_self.endGame) {
+                if (!_self.fieldCells) return _self.createField(currentCell, this);
+                _self.checkCell(currentCell, this);
+            }
+        }
+
+        bindRightClick(...self) {
+            const _self = self[0];
+            const currentCell = {x: +this.id.split('-')[0], y: +this.id.split('-')[1]};
+            if (!_self.endGame) {
+                if (!_self.fieldCells) return _self.createField(currentCell, this);
+                _self.addFlag(currentCell, this);
+            }
+        }
+
+        createField(firstCell, firstElement) {
+            const _self = this;
+            const fieldHeight = _self.fieldHeight;
+            const fieldWidth = _self.fieldWidth;
+            _self.unCheckedCells = fieldHeight * fieldWidth;
+            _self.fieldCells = {};
+            let indexBombs = [];
+
+            const setIndexCells = function () {
+                for (let i = 0; i < fieldHeight; i++) {
+                    for (let j = 0; j < fieldWidth; j++) {
+                        indexBombs.push({x: i, y: j});
+                        _self.fieldCells[`x: ${i}, y: ${j}`] = {
+                            x: i,
+                            y: j,
+                            isBomb: false,
+                            isChecked: false,
+                            count: null,
+                            isFlag: false,
+                        }
+                    }
+                }
+            }();
+
+            const setIndexBombs = function () {
+                indexBombs = indexBombs.filter(function (cell) {
+                    return JSON.stringify(cell) !== JSON.stringify(firstCell);
+                });
+
+                function compareRandom() {
+                    return Math.random() - 0.5;
+                }
+
+                indexBombs.sort(compareRandom).splice(_self.bombNumber);
+
+                indexBombs.forEach(function (cell) {
+                    _self.fieldCells[`x: ${cell.x}, y: ${cell.y}`].isBomb = true;
+                });
+            }();
+
+            const setActiveCells = function () {
+                let currentCell;
+                indexBombs.forEach(function (cell) {
+                    for (let i = cell.x - 1; i <= cell.x + 1; i++) {
+                        for (let j = cell.y - 1; j <= cell.y + 1; j++) {
+                            currentCell = _self.fieldCells[`x: ${i}, y: ${j}`];
+                            if (currentCell) {
+                                if (!currentCell.isBomb) {
+                                    currentCell.count = currentCell.count || 0;
+                                    currentCell.count++;
+                                }
+                            }
+                        }
+                    }
+                });
+            }();
+
+            const setEmptyCells = function () {
+                let fieldCells = _self.fieldCells;
+                for (let key in fieldCells) {
+                    if (!fieldCells[key].count && !fieldCells[key].isBomb) {
+
+                        fieldCells[key].nearEmptyCells = {};
+                        fieldCells[key].nearCountCells = {};
+
+                        for (let i = fieldCells[key].x - 1; i <= fieldCells[key].x + 1; i++) {
+                            for (let j = fieldCells[key].y - 1; j <= fieldCells[key].y + 1; j++) {
+                                if (fieldCells[`x: ${i}, y: ${j}`] && !fieldCells[`x: ${i}, y: ${j}`].isBomb) {
+                                    if (!fieldCells[`x: ${i}, y: ${j}`].count) {
+                                        fieldCells[key].nearEmptyCells[`x: ${i}, y: ${j}`] = {
+                                            x: i,
+                                            y: j
+                                        };
+                                    } else {
+                                        fieldCells[key].nearCountCells[`x: ${i}, y: ${j}`] = {
+                                            x: i,
+                                            y: j
+                                        };
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (let key in fieldCells) {
+                    if (!fieldCells[key].count && !fieldCells[key].isBomb) {
+                        for (let item in fieldCells[key].nearEmptyCells) {
+                            let currentCell = fieldCells[key].nearEmptyCells[item];
+
+                            Object.assign(fieldCells[`x: ${currentCell.x}, y: ${currentCell.y}`].nearEmptyCells, fieldCells[key].nearEmptyCells);
+                            fieldCells[key].nearEmptyCells = fieldCells[`x: ${currentCell.x}, y: ${currentCell.y}`].nearEmptyCells;
+
+                            Object.assign(fieldCells[`x: ${currentCell.x}, y: ${currentCell.y}`].nearCountCells, fieldCells[key].nearCountCells);
+                            fieldCells[key].nearCountCells = fieldCells[`x: ${currentCell.x}, y: ${currentCell.y}`].nearCountCells;
+                        }
+                    }
+                }
+            }();
+
+            _self.checkCell(firstCell, firstElement);
+        }
+
+        checkCell(currentCell, currentElement) {
+            const _self = this;
+            let _currentCell = _self.fieldCells[`x: ${currentCell.x}, y: ${currentCell.y}`];
+
+            if (!_currentCell.isFlag) {
+                if (_currentCell.isBomb) {
+                    _self.finishGame(false);
+                } else {
+                    if (!currentElement.isChecked) {
+                        currentElement.classList.add('is-safe');
+                        if (_currentCell.count) {
+                            currentElement.innerHTML = _currentCell.count;
+                            _currentCell.isChecked = true;
+                            _self.unCheckedCells--;
+                        } else {
+                            _currentCell.isChecked = true;
+
+                            for (let key in _currentCell.nearEmptyCells) {
+                                let nearEmptyCell = _self.fieldCells[`x: ${_currentCell.nearEmptyCells[key].x}, y: ${_currentCell.nearEmptyCells[key].y}`];
+                                let nearEmptyElement = document.getElementById(`${_currentCell.nearEmptyCells[key].x}-${_currentCell.nearEmptyCells[key].y}`);
+                                nearEmptyCell.isChecked = true;
+                                nearEmptyCell.isFlag = false;
+                                _self.unCheckedCells--;
+                                nearEmptyElement.classList.remove('is-flag');
+                                nearEmptyElement.classList.add('is-safe');
+                            }
+
+                            for (let key in _currentCell.nearCountCells) {
+                                let nearCountCell = _self.fieldCells[`x: ${_currentCell.nearCountCells[key].x}, y: ${_currentCell.nearCountCells[key].y}`];
+                                let nearCountElement = document.getElementById(`${_currentCell.nearCountCells[key].x}-${_currentCell.nearCountCells[key].y}`);
+                                if (!nearCountCell.isChecked) {
+                                    nearCountCell.isChecked = true;
+                                    nearCountCell.isFlag = false;
+                                    _self.unCheckedCells--;
+                                }
+                                nearCountElement.classList.remove('is-flag');
+                                nearCountElement.classList.add('is-safe');
+                                nearCountElement.innerHTML = nearCountCell.count;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (_self.unCheckedCells === _self.bombNumber) {
+                _self.finishGame(true);
+            }
+        }
+
+        addFlag(currentCell, currentElement) {
+            const _self = this;
+            let _currentCell = _self.fieldCells[`x: ${currentCell.x}, y: ${currentCell.y}`];
+
+            if(!_currentCell.isChecked) {
+                if (!_currentCell.isFlag) {
+                    _currentCell.isFlag = true;
+                    currentElement.classList.add('is-flag');
+                } else {
+                    _currentCell.isFlag = false;
+                    currentElement.classList.remove('is-flag');
+                }
+            }
+        }
+
+        finishGame(isWin) {
+            let _self = this;
+            _self.endGame = true;
+
+            for (let key in _self.fieldCells) {
+                let fieldCells = _self.fieldCells[key];
+                if (fieldCells.isBomb) {
+                    document.getElementById(`${fieldCells.x}-${fieldCells.y}`).classList.remove('is-flag');
+                    document.getElementById(`${fieldCells.x}-${fieldCells.y}`).classList.add('is-bomb');
+                }
+            }
+
+            if (isWin) {
+                alert('Win');
+            } else {
+                alert('Lose');
+            }
+        }
+    }
+
+    return {
+        init: function () {
+            const _self = this;
+
+            const defaultBtn = document.getElementsByClassName('j-btn');
+
+            Array.from(defaultBtn).forEach(function (element) {
+                element.addEventListener('click', _self.createGameField);
+            });
+
+        },
+        createGameField: function () {
+            let fieldWidth, fieldHeight, bombNumber;
+            const currentBtn = this;
+            const inputWidth = document.getElementsByName('fieldWidth')[0];
+            const inputHeight = document.getElementsByName('fieldHeight')[0];
+            const inputBombs = document.getElementsByName('bombs')[0];
+            const fieldContainer = document.getElementById('field');
+
+            if (currentBtn.dataset.custom) {
+                fieldWidth = Math.round(!+inputWidth.value || +inputWidth.value <= 8 ? 8 : (+inputWidth.value > 40 ? 40 : +inputWidth.value));
+                fieldHeight = Math.round(!+inputHeight.value || +inputHeight.value <= 8 ? 8 : (+inputHeight.value > 40 ? 40 : +inputHeight.value));
+                bombNumber = Math.round(+inputBombs.value || fieldWidth * fieldHeight * 0.1);
+                bombNumber = bombNumber < Math.round(fieldWidth * fieldHeight * 0.3) ? bombNumber : Math.round(fieldWidth * fieldHeight * 0.3)
+            } else {
+                switch (currentBtn.dataset.level) {
+                    case 'easy':
+                        fieldWidth = 9;
+                        fieldHeight = 9;
+                        bombNumber = 10;
+                        break;
+                    case 'medium':
+                        fieldWidth = 16;
+                        fieldHeight = 16;
+                        bombNumber = 40;
+                        break;
+                    case 'hard':
+                        fieldWidth = 30;
+                        fieldHeight = 16;
+                        bombNumber = 99;
+                        break;
+                    default:
+                        fieldWidth = 9;
+                        fieldHeight = 9;
+                        bombNumber = 10;
+                }
+            }
+
+            inputWidth.value = fieldWidth;
+            inputHeight.value = fieldHeight;
+            inputBombs.value = bombNumber;
+
+            fieldContainer.innerHTML = '';
+            fieldContainer.classList.remove('is-small');
+
+            if (fieldWidth <= 16 && fieldHeight <= 16) {
+                fieldContainer.classList.add('is-small');
+            }
+
+            game = new GameField(fieldWidth, fieldHeight, bombNumber);
+
+            for (let i = 0; i < fieldHeight; i++) {
+                const fieldRowContainer = document.createElement('div');
+                fieldRowContainer.className = 'field__row';
+                fieldContainer.appendChild(fieldRowContainer)
+                for (let j = 0; j < fieldWidth; j++) {
+                    const fieldCellContainer = document.createElement('button');
+                    fieldCellContainer.className = 'field__btn';
+                    fieldCellContainer.setAttribute('id', `${i}-${j}`);
+                    fieldRowContainer.appendChild(fieldCellContainer);
+
+                    fieldCellContainer.addEventListener('click', function () {
+                        game.bindClick.call(this, game);
+                    });
+                    fieldCellContainer.addEventListener('contextmenu', function (e) {
+                        e.preventDefault();
+                        game.bindRightClick.call(this, game);
+                        return false;
+                    }, false);
+                }
+            }
+        }
+    };
+})();
+
+(function () {
+    MineSweeper.init();
+})();
---
+++ js/main.js
@@ -0,0 +1,194 @@
+'use strict';
+
+const bombNum = 10;
+const fieldWidth = 20;
+const fieldHeight = 10;
+
+let cellCount = fieldWidth * fieldHeight;
+
+let field = {};
+let indexCells = [];
+let indexBombs = [];
+let indexActiveCell = {};
+let indexEmpty = [];
+
+const fieldContainer = document.getElementById('field');
+
+const getIndexCells = function () {
+    for (let i = 0; i < fieldHeight; i++) {
+        for (let j = 0; j < fieldWidth; j++) {
+            indexCells.push({x: i, y: j});
+        }
+    }
+}();
+
+const getIndexBombs = function (cellFirst) {
+    indexBombs = indexCells.slice(0);
+
+    indexBombs = indexBombs.filter(function (item) {
+        return JSON.stringify(item) !== JSON.stringify(cellFirst);
+    });
+
+    function compareRandom(a, b) {
+        return Math.random() - 0.5;
+    }
+
+    indexBombs.sort(compareRandom).splice(bombNum);
+};
+
+const setActiveCells = function () {
+    let indexActiveCellTemp = indexBombs.map(function (item) {
+        return {
+            x: item.x,
+            y: item.y,
+            isBomb: true,
+        };
+    });
+
+    console.log(indexActiveCellTemp);
+
+
+    indexBombs.forEach(function (item) {
+        for (let i = item.x - 1; i <= item.x + 1; i++) {
+            for (let j = item.y - 1; j <= item.y + 1; j++) {
+                if (!(i < 0 || j < 0 || i >= fieldHeight || j >= fieldWidth) && !(item.x === i && item.y === j)) {
+                    indexActiveCellTemp.push({x: i, y: j, isBomb: false, count: 1});
+                }
+            }
+        }
+    });
+
+    console.log(indexActiveCellTemp);
+
+    function compareIndex(a, b) {
+        return a.x - b.x || a.y - b.y;
+    }
+
+    indexActiveCellTemp.sort(compareIndex);
+
+    let privious;
+
+    indexActiveCellTemp.forEach(function (item) {
+        if (!privious || !(privious.x === item.x && privious.y === item.y)) {
+            indexActiveCell[`x: ${item.x}, y: ${item.y}`] = {
+                'isBomb': item.isBomb,
+                'count': item.count
+            };
+        } else {
+            if (!privious.isBomb && item.count) {
+                indexActiveCell[`x: ${item.x}, y: ${item.y}`].count++;
+            }
+        }
+        privious = item;
+    });
+};
+
+const checkEmptyCell = function (currentCell) {
+
+
+   // console.log(currentCell);
+
+    for (let i = currentCell.x - 1; i <= currentCell.x + 1; i++) {
+        for (let j = currentCell.y - 1; j <= currentCell.y + 1; j++) {
+            if (!(i < 0 || j < 0 || i >= fieldHeight || j >= fieldWidth)) {
+
+
+                    let activeCell = document.getElementById(`${i}-${j}`);
+
+                    activeCell.classList.add("is-safe");
+                    activeCell.removeEventListener('click', getStatusCell);
+
+
+                    let flag = indexEmpty.some(function (item) {
+                        return (item.x === i && item.y === j);
+                    });
+
+                    if (!flag) {
+                        if (`x: ${i}, y: ${j}` in indexActiveCell) {
+                            if(!activeCell.innerHTML) {
+                                activeCell.innerHTML += indexActiveCell[`x: ${i}, y: ${j}`].count;
+                                cellCount--;
+                            }
+                        } else {
+                            indexEmpty.push({x: i, y: j, isChecked: false});
+                        }
+
+                    }
+
+            }
+        }
+    }
+
+    for (let item of indexEmpty) {
+        if (!item.isChecked) {
+            item.isChecked = true;
+            checkEmptyCell({'x': item.x, 'y': item.y});
+            break;
+        }
+    }
+
+    cellCount--;
+
+    //console.log(cellCount);
+
+};
+
+const getStatusCell = function () {
+    console.log('click');
+
+
+    let _self = this;
+    let x = +_self.id.split('-')[0];
+    let y = +_self.id.split('-')[1];
+    let currentCoords = `x: ${x}, y: ${y}`;
+    let currentCoordsObJ = {'x': x, 'y': y};
+
+    if (fieldContainer.dataset.start) {
+        if (indexActiveCell[currentCoords]) {
+            if (indexActiveCell[currentCoords].isBomb) {
+                _self.classList.add("is-bomb");
+            } else {
+                _self.classList.add("is-safe");
+                _self.innerHTML = indexActiveCell[currentCoords].count;
+                cellCount--;
+                indexEmpty.push({'x': x, 'y': y, isChecked: true});
+            }
+        } else {
+            _self.classList.add("is-safe");
+            indexEmpty.length = 0;
+            checkEmptyCell(currentCoordsObJ);
+        }
+
+    } else {
+        _self.classList.add("is-safe");
+        fieldContainer.dataset.start = true;
+        getIndexBombs(currentCoordsObJ);
+        setActiveCells();
+        if (indexActiveCell[currentCoords]) {
+            _self.innerHTML = indexActiveCell[currentCoords].count;
+            cellCount--;
+            indexEmpty.push({'x': x, 'y': y, isChecked: true});
+        } else {
+            indexEmpty.length = 0;
+            checkEmptyCell(currentCoordsObJ);
+        }
+    }
+
+    _self.removeEventListener('click', getStatusCell);
+
+   // console.log(indexEmpty);
+};
+
+for (var i = 0; i < fieldHeight; i++) {
+    const fieldRowContainer = document.createElement('div');
+    fieldRowContainer.className = 'field__row';
+    fieldContainer.appendChild(fieldRowContainer);
+    for (var j = 0; j < fieldWidth; j++) {
+        const fieldCellContainer = document.createElement('button');
+        fieldCellContainer.className = 'field__btn';
+        fieldCellContainer.setAttribute('id', `${i}-${j}`);
+        fieldCellContainer.addEventListener('click', getStatusCell);
+        fieldRowContainer.appendChild(fieldCellContainer);
+    }
+}
+
